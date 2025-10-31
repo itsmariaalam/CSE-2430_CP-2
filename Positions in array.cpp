af919506a2bf7afc479f8f70a00943a4a9d78a5e
Maria Alam <memariaalam@gmail.com>
2025-11-01 00:00:39 +0600

    Create Positions in array.cpp

---
+++ Positions in array.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int a[1005];
+    for (int i = 0; i < n; i++) cin >> a[i];
+
+    for (int i = 0; i < n; i++) {
+        if (a[i] <= 10) {
+            cout << "A[" << i << "] = " << a[i] << endl;
+        }
+    }
+
+    return 0;
+}
