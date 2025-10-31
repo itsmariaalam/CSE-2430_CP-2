ae8437f46d26d57c0fc860e64838ea5c1f3683d3
Maria Alam <memariaalam@gmail.com>
2025-10-31 23:46:08 +0600

    Create summing.cpp

---
+++ summing.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>   
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    long long sum = 0, x;
+
+    for (int i = 0; i < n; i++) {
+        cin >> x;
+        sum += x;
+    }
+
+    cout << abs(sum) << endl; 
+    return 0;
+}
