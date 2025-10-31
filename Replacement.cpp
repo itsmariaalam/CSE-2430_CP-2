67539ba2b3d5e7a34b653188fd03e31ec0556a1d
Maria Alam <memariaalam@gmail.com>
2025-10-31 23:58:19 +0600

    Create Replacement.cpp

---
+++ Replacement.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int a[1005];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+        if (a[i] > 0) a[i] = 1; 
+        else if (a[i] < 0) a[i] = 2; 
+    }
+    for (int i = 0; i < n; i++) cout << a[i] << " ";
+    cout << endl;
+    return 0;
+}
